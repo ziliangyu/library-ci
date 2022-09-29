7d9b5b4a851a9d0ef5e2d428cfabf42e7605f518
yuziliang <yuziliangvictory@gmail.com>
2022-09-29 22:41:52 +0800

    feat: test

--- app/src/main/java/dev/shreyaspatil/example/MainActivity.kt
+++ app/src/main/java/dev/shreyaspatil/example/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Greeter.greet("John Dou")
+        Greeter.greet("John DoO")
     }
 }
